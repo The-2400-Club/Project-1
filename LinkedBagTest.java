e629788ee4daa899e59f443011e38b38cdd25fea
Jonathan-Anz <98674816+Jonathan-Anz@users.noreply.github.com>
2022-02-27 13:41:47 -0800

    Added logic for LinkedBagTest

--- LinkedBagTest.java
+++ LinkedBagTest.java
@@ -13,14 +13,30 @@ public class LinkedBagTest
         bag2.add("d");
         bag2.add("e");
 
-        //BagInterface<String> bag3 = bag1.union(bag2);
-        //BagInterface<String> bag4 = bag1.intersection(bag2);
+        BagInterface<String> bag3 = bag1.union(bag2);
+        BagInterface<String> bag4 = bag1.intersection(bag2);
         BagInterface<String> bag5 = bag1.difference(bag2);
 
-        Object[] bagArray = bag5.toArray();
-        for (int i = 0; i < bagArray.length; i++)
+        Object[] bagArray3 = bag3.toArray();
+        for (int i = 0; i < bagArray3.length; i++)
         {
-           System.out.print(bagArray[i] + " ");
+           System.out.print(bagArray3[i] + " ");
+        }
+
+        System.out.println();
+
+        Object[] bagArray4 = bag4.toArray();
+        for (int i = 0; i < bagArray4.length; i++)
+        {
+           System.out.print(bagArray4[i] + " ");
+        }
+
+        System.out.println();
+
+        Object[] bagArray5 = bag5.toArray();
+        for (int i = 0; i < bagArray5.length; i++)
+        {
+           System.out.print(bagArray5[i] + " ");
         }
     }
 }
